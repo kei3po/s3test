ebd91d8d087fe740a9d45ba8d1f93b44922d9088
kei3po <nielsen.keith@gmail.com>
2016-12-12 13:30:19 -0500

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -e "AWS_ACCESS_KEY_ID=AKIAJHPNYFD7O2I7ELFA" -e "AWS_SECRET_ACCESS_KEY=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" -i -t coredev/s3test:v3 /bin/bash
+docker run -e "AWS_ACCESS_KEY_ID=" -e "AWS_SECRET_ACCESS_KEY=" -i -t coredev/s3test:v3 /bin/bash
